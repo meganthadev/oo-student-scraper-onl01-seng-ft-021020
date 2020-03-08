20d8d34deee7fdd33bf0ab4d43faf5a76a12db85
Megan Horsfield <meganthadev@gmail.com>
2020-03-08 02:14:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
+    html = open(index_url)
     doc = Nokogiri::HTML (open("https://learn-co-curriculum.github.io/student-scraper-test-page/"))
   end
 
