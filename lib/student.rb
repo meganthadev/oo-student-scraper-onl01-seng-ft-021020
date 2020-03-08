c057d7e6f4a04b4a9489429975f668f15a401cdf
Megan Horsfield <meganthadev@gmail.com>
2020-03-08 02:24:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,10 +9,10 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each do |x| 
-       Student.new(student_hash)
-    end
+    students_array.each do |hash| 
+       Student.new(hash)
   end
+
 
   def add_student_attributes(attributes_hash)
     
